bc2c65f7cb298ce1a3ec2ef0d02b97aeb8a1d2b7
Admin <admin@milads-MacBook-Pro.local>
2017-07-29 00:27:04 -0400

    friend finder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 
 var app=express();
 
-var port=process.env.port || 8080;
+var port=process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
